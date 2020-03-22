4d64277ced5a4b06650153259e96f55dbac48017
PLEUMKUNG <sopanus2014@gmail.com>
2020-03-22 19:57:11 +0700

    Update Game Server Version

--- Common/Constants/ServerConstants.cs
+++ Common/Constants/ServerConstants.cs
@@ -1,4 +1,4 @@
-﻿namespace Server.Common.Constants
+namespace Server.Common.Constants
 {
     public class ServerConstants
     {
@@ -6,7 +6,7 @@
         public static readonly bool AUTO_REGISTRATION = false;
         public static readonly int CHANNEL_DEFAULT = 18;
         public static readonly int CHANNEL_LOAD = 300;
-        public static readonly int CLIENT_VERSION = 202003041;  // For TH Client Only
+        public static readonly int CLIENT_VERSION = 202003182;  // For TH Client Only
         public static readonly bool PASSWORD_DECODE = false;
         public static readonly int UDP_PORT = 15199;
     }
