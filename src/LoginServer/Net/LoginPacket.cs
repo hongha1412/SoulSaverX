c8cb2e8cebdf8d16135e986d451b93e70dae641c
PLEUMKUNG <sopanus2014@gmail.com>
2020-03-25 02:32:01 +0700

    Update Game Version

--- src/LoginServer/Net/LoginPacket.cs
+++ src/LoginServer/Net/LoginPacket.cs
@@ -12,7 +12,7 @@ namespace Server.Ghost
             using (var plew = new OutPacket())
             {
                 plew.WriteHexString("AA 55 2F 00 11"); // Packet Header
-                plew.WriteInt(2020032501); // Patch Version 2020 / 03 / 03 / 03 --> V
+                plew.WriteInt(2020032502); // Patch Version 2020 / 03 / 03 / 03 --> V
                 plew.WriteHexString("00 00 00 00");
                 plew.WriteString("http://ghost.pleum.in.th/");
                 plew.WriteString("file/test");
