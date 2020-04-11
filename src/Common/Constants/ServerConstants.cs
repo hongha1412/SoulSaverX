2345c2b3ac6b8d5d75a2cca2b7668c2c06206e30
PLEUMKUNG <sopanus2014@gmail.com>
2020-04-11 22:56:55 +0700

    Update ServerConstants.cs

--- src/Common/Constants/ServerConstants.cs
+++ src/Common/Constants/ServerConstants.cs
@@ -3,11 +3,11 @@ namespace Server.Common.Constants
     public class ServerConstants
     {
 
-        public static readonly int CLIENT_VERSION = 202003191;
+        public static readonly int CLIENT_VERSION = 202003122;
 
         public static readonly string SERVER_IP = "127.0.0.1";
         public static readonly bool AUTO_REGISTRATION = false;
-        public static readonly int CHANNEL_DEFAULT = 18;
+        public static readonly int CHANNEL_DEFAULT = 8;
         public static readonly int CHANNEL_LOAD = 300;
         public static readonly bool PASSWORD_DECODE = false;
         public static readonly int UDP_PORT = 15199;
